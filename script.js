0f17abde084f6d1ba7386bb2eb1d0eae5a3186eb
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-01-07 13:24:33 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var count = 9;
+function printMessage(){
+  count ++;
+
+  var increment = document.querySelector
+  (".increment");
+  console.log(increment.innerText);
+  increment.innerText = count + " like (s)"
+}
+
+var count = 12;
+function printMessage1(){
+  count ++;
+
+  var increment1 = document.querySelector
+  (".increment1");
+  console.log(increment1.innerText);
+  increment1.innerText = count + " like (s)"
+}
+
+var count = 9;
+function printMessage2(){
+  count ++;
+
+  var increment2 = document.querySelector
+  (".increment2");
+  console.log(increment2.innerText);
+  increment2.innerText = count + " like (s)"
+}
